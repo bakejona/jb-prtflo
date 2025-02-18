0c69a43213f223a576ed7e0da680a27029071dae
bakejona <jbakerbba2@gmail.com>
2025-02-18 15:32:46 -0500

    added links for github and linked in added media querys for mobile and tablet

--- src/app/AboutSection.js
+++ src/app/AboutSection.js
@@ -1,7 +1,9 @@
 import styles from './AboutSection.module.css';
 import Image from 'next/image';
+import Head from 'next/head';
 
 
+//adding logos
 const skills = [
   { name: 'HTML', logo: '/logos/html.svg' },
   { name: 'CSS', logo: '/logos/css.svg' },
@@ -13,19 +15,36 @@ const skills = [
   { name: 'Adobe Illustrator', logo: '/logos/adobe-illustrator.svg' },
 ];
 
+//media links
 const AboutSection = () => {
+  const githubLink = 'https://github.com/bakejona';
+  const linkedinLink = 'https://www.linkedin.com/in/jonathan-baker-5291351b5/';
+
+<Head>
+<title>&#128075; Jonathan</title>
+</Head>
   return (
     <section id="about" className={styles.section}>
       <div className={styles.container}>
         <div className={styles.textContainer}>
           <div className={styles.maskingContainer}>
-            <h1 className={styles.header}>&#128075; Hi, I'm Jonathan</h1>
+            <h1 className={styles.header}><span>&#128075;</span> Hi, I'm Jonathan</h1>
           </div>
           <p className={styles.paragraph}>
             I am a UX/UI designer, developer, and passionate about creating user-centered experiences.
           </p>
         </div>
       </div>
+      
+      <div className={styles.socialLinks}>
+        <a href={githubLink} >
+          <Image src="/logos/github.svg" alt="GitHub" className={styles.socialLogo} width={40} height={40} />
+        </a>
+        <a href={linkedinLink} >
+          <Image src="/logos/linkedin.svg" alt="LinkedIn" className={styles.socialLogo} width={40} height={40} />
+        </a>
+      </div>
+
       <div className={styles.container}>
         <div className={styles.mySkills}>
           <h1>My Skills</h1>

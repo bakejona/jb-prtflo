8bebf46f3babca8344ebd3061289dc00fb4ce8a7
bakejona <jbakerbba2@gmail.com>
2025-02-18 17:24:37 -0500

    - Added email system directed towards primary email - Styled contact form

--- src/app/ContactSection.js
+++ src/app/ContactSection.js
@@ -1,12 +1,88 @@
-// ContactSection.js
+"use client"
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import styles from './ContactSection.module.css';
+
 const ContactSection = () => {
-    return (
-      <section id="contact">
-        <h1>Contact Section</h1>
-        <p>Here you can provide contact details or a contact form.</p>
-      </section>
-    );
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const formData = {
+      name,
+      email,
+      message,
+    };
+
+    emailjs
+      .send(
+        'service_xqukd3x',
+        'template_l85jbfn',
+        formData,
+        'frXLL8taZu9OlOKfW'
+      )
+      .then(
+        (result) => {
+          console.log('SUCCESS!', result.text);
+          setIsSent(true);
+          setName('');
+          setEmail('');
+          setMessage('');
+        },
+        (error) => {
+          console.error('FAILED...', error.text);
+          setIsSent(false);
+        }
+      )
+      .finally(() => {
+        setIsSubmitting(false);
+      });
   };
-  
-  export default ContactSection;
-  
+
+  return (
+    <section id="contact" className={styles.section}>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <div className={styles.formText}>
+          <h1>Contact Me 📝</h1>
+          <p>Have any questions or want to get in touch? Feel free to reach out!</p>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <textarea
+          placeholder="Your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        ></textarea>
+        
+        <button type="submit" className={styles.submitButton} disabled={isSubmitting}>
+          {isSubmitting ? 'Sending...' : isSent ? 'Sent! ✅' : 'Send'}
+        </button>
+      </form>
+      
+      {responseMessage && <p>{responseMessage}</p>}
+    </section>
+  );
+};
+
+export default ContactSection;
